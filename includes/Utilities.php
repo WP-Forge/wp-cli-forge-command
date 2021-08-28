bb0d0284b7069a8bd1761fc9afb80bf729ed8eda
Micah Wood <micah@wpscholar.com>
2021-08-28 09:54:37 -0400

    Add utility to properly resolve paths that don't exist yet

--- includes/Utilities.php
+++ includes/Utilities.php
@@ -27,13 +27,14 @@ class Utilities {
 			}
 			$result[ implode( '.', $keys ) ] = $value;
 		}
+
 		return $result;
 	}
 
 	/**
 	 * Transform a value via a callable.
 	 *
-	 * @param mixed    $value Value to be transformed
+	 * @param mixed    $value    Value to be transformed
 	 * @param callable $callable Callable to be called.
 	 *
 	 * @return mixed
@@ -43,13 +44,14 @@ class Utilities {
 		if ( method_exists( Transforms::class, $callable ) ) {
 			return call_user_func( array( Transforms::class, $callable ), $value );
 		}
+
 		return $callable( $value );
 	}
 
 	/**
 	 * Apply one or more transforms to a value, in order.
 	 *
-	 * @param mixed          $value Value to be transformed
+	 * @param mixed          $value      Value to be transformed
 	 * @param array|callable $transforms Transforms to be called.
 	 *
 	 * @return mixed
@@ -63,7 +65,42 @@ class Utilities {
 				$value = self::transform( $value, $callable );
 			}
 		}
+
 		return $value;
+	}
+
+	/**
+	 * Resolve a path. Like realpath(), but works for non-existent files and directories.
+	 *
+	 * @param $path
+	 *
+	 * @return false|mixed|string
+	 */
+	public static function resolvePath( $path ) {
+		if ( DIRECTORY_SEPARATOR !== '/' ) {
+			$path = str_replace( DIRECTORY_SEPARATOR, '/', $path );
+		}
+		$search = explode( '/', $path );
+		$search = array_filter( $search, function ( $part ) {
+			return $part !== '.';
+		} );
+		$append = array();
+		$match  = false;
+		while ( count( $search ) > 0 ) {
+			$match = realpath( implode( '/', $search ) );
+			if ( $match !== false ) {
+				break;
+			}
+			array_unshift( $append, array_pop( $search ) );
+		};
+		if ( $match === false ) {
+			$match = getcwd();
+		}
+		if ( count( $append ) > 0 ) {
+			$match .= DIRECTORY_SEPARATOR . implode( DIRECTORY_SEPARATOR, $append );
+		}
+
+		return $match;
 	}
 
 }
